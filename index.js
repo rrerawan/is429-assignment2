9cb8be51497a1bf096d46fa0b25c825d758cd992
Ricky Erawan <rrerawan.2012@sis.smu.edu.sg>
2015-03-10 08:20:28 +0800

    n

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var express = require('express');
+var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -9,8 +11,7 @@ app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
 
-var express = require('express');
-var app = express();
+
 var model = require('./model.js');
 var paypal = require('paypal-rest-sdk');
 
